f79d55d2c5efa1e28f4707483ea25e6fe0eccf03
oukalatte <oukalatte@gmail.com>
2023-03-28 16:51:34 +0800

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import "@/styles/style.scss";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { Noto_Sans_JP } from "next/font/google";
+const font = Noto_Sans_JP({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--noto-font",
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // new Contents(document.querySelector(".l-canvas"));
+  }, []);
+  return (
+    <main className={font.variable}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
