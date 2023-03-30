7e13d1528d1f5727edc6afd48373530d0c73984c
oukalatte <oukalatte@gmail.com>
2023-03-30 22:02:44 +0800

    webgl background

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <title>Ariel の励ましの言葉</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
