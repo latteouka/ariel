17bcb07ee42f0ce10ad0e5386bcedf5eb5170f3b
oukalatte <oukalatte@gmail.com>
2023-05-09 09:16:02 +0800

    update

--- utils/useLenis.ts
+++ utils/useLenis.ts
@@ -7,6 +7,7 @@ const useLenis = () => {
 
   useEffect(() => {
     lenis.current = new Lenis({
+      duration: 0.8,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
       smoothTouch: true,
       touchMultiplier: 3,
